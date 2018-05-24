8ee6f218d831eab188593ed5098b8b6f15c91cdd
mjr390 <mjr117@yahoo.com>
2018-05-24 11:52:21 -0400

    create list to hole converted state names to variables

--- PyBoss/main.py
+++ PyBoss/main.py
@@ -12,6 +12,60 @@ lastNames = []
 DOB = []
 SSNall = []
 ID = []
+stateApp = []
+us_state_abbrev = {
+    'Alabama': 'AL',
+    'Alaska': 'AK',
+    'Arizona': 'AZ',
+    'Arkansas': 'AR',
+    'California': 'CA',
+    'Colorado': 'CO',
+    'Connecticut': 'CT',
+    'Delaware': 'DE',
+    'Florida': 'FL',
+    'Georgia': 'GA',
+    'Hawaii': 'HI',
+    'Idaho': 'ID',
+    'Illinois': 'IL',
+    'Indiana': 'IN',
+    'Iowa': 'IA',
+    'Kansas': 'KS',
+    'Kentucky': 'KY',
+    'Louisiana': 'LA',
+    'Maine': 'ME',
+    'Maryland': 'MD',
+    'Massachusetts': 'MA',
+    'Michigan': 'MI',
+    'Minnesota': 'MN',
+    'Mississippi': 'MS',
+    'Missouri': 'MO',
+    'Montana': 'MT',
+    'Nebraska': 'NE',
+    'Nevada': 'NV',
+    'New Hampshire': 'NH',
+    'New Jersey': 'NJ',
+    'New Mexico': 'NM',
+    'New York': 'NY',
+    'North Carolina': 'NC',
+    'North Dakota': 'ND',
+    'Ohio': 'OH',
+    'Oklahoma': 'OK',
+    'Oregon': 'OR',
+    'Pennsylvania': 'PA',
+    'Rhode Island': 'RI',
+    'South Carolina': 'SC',
+    'South Dakota': 'SD',
+    'Tennessee': 'TN',
+    'Texas': 'TX',
+    'Utah': 'UT',
+    'Vermont': 'VT',
+    'Virginia': 'VA',
+    'Washington': 'WA',
+    'West Virginia': 'WV',
+    'Wisconsin': 'WI',
+    'Wyoming': 'WY',
+}
+
 
 #open the file reading each line as a new line in csv format
 with open(csvpath, newline='') as csvfile:
@@ -25,14 +79,19 @@ with open(csvpath, newline='') as csvfile:
         firstNames.append(splitNames[0][0])
         lastNames.append(splitNames[0][1])
         splitNames.pop(0)
+        
         DOB.append(datetime.strptime(row[2], '%Y-%m-%d').strftime('%m/%d/%Y'))
+        
         secretSSN = list(row[3])
         secretSSN[0:3] = '***'
         secretSSN[4:6] = "**"
         secretSSNstr = ''.join(secretSSN)
         SSNall.append(secretSSNstr)
+        
         ID.append(row[0])
 
+        stateApp.append(us_state_abbrev[row[4]])
+       
 
 
 
@@ -41,4 +100,5 @@ with open(csvpath, newline='') as csvfile:
 #print(DOB)
 #print(SSNall)
 #print(ID)
+#print(stateApp)
 
