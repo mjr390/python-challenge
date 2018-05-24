339f6528174f5763493f64142e5ed1b724a43911
mjr390 <mjr117@yahoo.com>
2018-05-24 09:44:23 -0400

    Fixed raw data to be correct files for challenge

--- PyBoss/main.py
+++ PyBoss/main.py
@@ -1,1 +1,6 @@
-Welcome
+#import needed packages
+import os
+import csv
+
+# create a variable of where the file is located, change for different files
+csvpath = os.path.join('..', 'PyBoss', 'employee_data_2.csv')
