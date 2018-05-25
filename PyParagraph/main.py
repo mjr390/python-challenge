9ec3ec722cb8280bd0a8dec470372e20fc52fa50
mjr390 <mjr117@yahoo.com>
2018-05-24 23:18:26 -0400

    added comments to the code so it can be easily understood later

--- PyParagraph/main.py
+++ PyParagraph/main.py
@@ -1,32 +1,42 @@
+#import needed packages
 import csv
 import os
 import re
 
-
-
+# create a variable of where the file is located, change for different files
 filepath = open("../PyParagraph/paragraph_2.txt", "r")
 
-x = filepath.read()
+#create a variable to read the file
+readText = filepath.read()
 
-wordCount = len(x.split())
+#find out the number of words by spliting the text based on spaces and assigning to a var
+wordCount = len(readText.split())
 #print(wordCount)
 
-sentenceCount = len(x.split('.'))
+#find the number of sentences by spliting the text based on "." and assigning to a var
+sentenceCount = len(readText.split('.'))
 #print(sentenceCount)
 
+#find the average number of words in a sentence by divding wordCount by sentenceCount and assign the result to a var
 avgSentenceLength = wordCount/sentenceCount
 #print(avgSentenceLength)
 
-# print(totalcom)
+#find the total number of letters by creating a variable and setting it to 0
 letterCount = 0
+
+#create a list of every letter to check if a character in the text is a letter
 alphabet=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','X','Y','Z']
-for char in x:
+for char in readText:
+    #if it is, add to letterCount to keep a running total of how many letter there are
     if char in alphabet:
         letterCount += 1
 #print(letterCount)
+#find the average letters per word by divding the total number of letters by the total words. Assign the result to a variable
 avgLetterCount = letterCount/wordCount
 #print(avgLetterCount)  
 
+
+#print the results to the console with some formatting
 print("Paragraph Analysis")
 print("-"*30)
 print("Approximate Word Count: " + str(wordCount))
@@ -34,11 +44,13 @@ print("Approximate Sentence Count: " + str(sentenceCount))
 print("Average Letter Count: " + str(avgLetterCount))
 print("Average Sentence Length: " + str(avgSentenceLength))      
 
-
+#Output the rezults to a new CSV file. 
+#MUST be a differnet file name from one that already exists   
 output_path = os.path.join("..", "PyParagraph", "output_2.csv")
 with open(output_path, 'w', newline='') as csvfile:
     csvwriter = csv.writer(csvfile, delimiter=',')
 
+    #write the lines in the new file
     csvwriter.writerow(["Paragraph Analysis", ""])
     csvwriter.writerow(["Approximate Word Count: ", str(wordCount)])
     csvwriter.writerow(["Approximate Sentence Count: ", str(sentenceCount)])
