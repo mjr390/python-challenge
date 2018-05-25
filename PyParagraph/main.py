988e1544534bdd311d63db42064e482f98f91d73
mjr390 <mjr117@yahoo.com>
2018-05-24 22:42:18 -0400

    removed commented out test code that isn't used

--- PyParagraph/main.py
+++ PyParagraph/main.py
@@ -2,13 +2,12 @@ import csv
 import os
 import re
 
-# senCount = 0
+
 
 filepath = open("../PyParagraph/paragraph_1.txt", "r")
-# with open(filepath, newline='.') as csvfile:
+
 x = filepath.read()
-#csv.filepath()
-#filepath.split('.')
+
 wordCount = len(x.split())
 print(wordCount)
 sentenceCount = len(x.split('.'))
@@ -16,35 +15,3 @@ print(sentenceCount)
 avgSentenceLength = wordCount/sentenceCount
 print(avgSentenceLength)
 
-
-
-
-
-
-
-#     for line in file:
-#         # print(len(line))
-#         #line.split(" ")
-#         filereader = (file, " ")
-#         for x in filereader:
-#             senCount += 1
-#     print(senCount)        
-
-
-
-# filepath = os.path.join('..', 'PyParagraph', 'paragraph_1.txt')
-
-# senCount = 0
-# #open the file reading each line as a new line in csv format
-# with open(filepath, newline='') as afile:
-#     #assign the read file to a variable separated by ','
-#     filereader = (afile, ".")
-#     print(filereader)
-#     #csv.reader(csvfile, delimiter=".")
-  
-# #     x = ' '
-#     for x in csvreader:
-#         senCount += 1#
-
-
-# print(senCount)        
