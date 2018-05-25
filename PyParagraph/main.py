7ddb0dc10fd7df24544ff2377f7ac5db9eb8917e
mjr390 <mjr117@yahoo.com>
2018-05-24 22:55:59 -0400

    added a list for the alphabet in order to count how many letters, created a var to hold avg letters/word

--- PyParagraph/main.py
+++ PyParagraph/main.py
@@ -10,8 +10,19 @@ x = filepath.read()
 
 wordCount = len(x.split())
 print(wordCount)
+
 sentenceCount = len(x.split('.'))
 print(sentenceCount)
+
 avgSentenceLength = wordCount/sentenceCount
 print(avgSentenceLength)
 
+# print(totalcom)
+letterCount = 0
+alphabet=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','X','Y','Z']
+for char in x:
+    if char in alphabet:
+        letterCount += 1
+#print(letterCount)
+avgLetterCount = letterCount/wordCount
+print(avgLetterCount)        
